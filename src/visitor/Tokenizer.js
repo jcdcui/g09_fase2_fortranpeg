ad82f3b1af9a0cb76e3f7b15e18cb9459cb8cbb5
Edmy20 <edmyingenieria@gmail.com>
2024-12-23 12:06:00 -0600

    arreglo de salida

--- src/visitor/Tokenizer.js
+++ src/visitor/Tokenizer.js
@@ -1,17 +1,30 @@
-import Visitor from './Visitor.js';
-import { Opciones, Rango, Union,Clase, Cadena } from './CST.js';
-import { Identificador } from './CST.js';
+import Visitor from "./Visitor.js";
+import { Opciones, Rango, Union, Clase, Cadena } from "./CST.js";
+import { Identificador } from "./CST.js";
 
 let concNum = 0;
-let cuantificar  = true;
-let currentId = ""
+let cuantificar = true;
+let currentId = "";
+let ruleName = "";
 export default class Tokenizer extends Visitor {
-    generateTokenizer(grammar) {
-        return `
-module tokenizer
+  generateTokenizer(grammar) {
+    return `
+module parser
 implicit none
 
 contains
+
+subroutine parse(input)
+    character(len=:), intent(inout), allocatable :: input
+    character(len=:), allocatable :: lexeme
+    integer :: cursor
+    cursor = 1
+    do while (lexeme /= "EOF" .and. lexeme /= "ERROR")
+        lexeme = nextSym(input, cursor)
+        print *, lexeme
+    end do
+end subroutine parse
+
 function nextSym(input, cursor) result(lexeme)
     character(len=*), intent(in) :: input
     integer, intent(inout) :: cursor
@@ -28,7 +41,7 @@ function nextSym(input, cursor) result(lexeme)
         return
     end if
 
-    ${grammar.map((produccion) => produccion.accept(this)).join('\n')}
+    ${grammar.map((produccion) => produccion.accept(this)).join("\n")}
 
     print *, "error lexico en col ", cursor, ', "' // input(cursor:cursor) // '"'
     lexeme = "ERROR"
@@ -48,106 +61,189 @@ function to_lower(str) result(lower_str)
         end if
     end do
 end function to_lower
-end module tokenizer
+
+function get_symbol(input_string) result(symbol)
+    character(len=*), intent(in) :: input_string
+    character(len=:), allocatable :: symbol
+
+    if (len(input_string) == 1) then
+      
+      select case (input_string)
+      case (' ')
+        symbol = '_'
+      case (char(9))   ! Tabulador
+        symbol = '\\t'
+      case (char(10))  ! Salto de línea
+        symbol = '\\n'
+      case (char(13))  ! Retorno de carro
+        symbol = '\\r'
+      case default
+        symbol = input_string
+      end select
+    else
+      
+      symbol = input_string
+    end if
+
+end function get_symbol
+
+end module parser
 
         `;
-    }
+  }
 
-    visitProducciones(node) {
-        if(node.alias){
-            currentId = node.alias.replace(/^"|"$/g, "");
-        }else{
-            currentId = node.id
-        }
-        return node.expr.accept(this);
-    }
-
-    visitOpciones(node) {
-        
-        return node.exprs.map((node) => node.accept(this)).join('\n');
-    }
-
-visitUnion(node){
-
-const nullQtyCount = node.exprs.filter(expr => expr.qty).length;
-let final = ""
+  visitProducciones(node) {
+    if (node.alias) {
+      ruleName = node.alias.replace(/^"|"$/g, "");
+      currentId = node.alias.replace(/^"|"$/g, "");
  
-        for (let i = 0; i < node.exprs.length-nullQtyCount; i++) 
-                { final += 'end if\n';}
+    } else {
+      ruleName = node.id;
+      currentId = node.id;
+    }
+    return node.expr.accept(this);
+  }
 
+  visitOpciones(node) {
+    return node.exprs.map((node) => node.accept(this)).join("\n");
+  }
 
-        let unirExp = `\nbufferConc = bufferConc // lexeme\n`;
-        let initConcat = `
+  visitUnion(node) {
+    const nullQtyCount = node.exprs.filter((expr) => expr.qty).length;
+    let final = "";
+
+    for (let i = 0; i < node.exprs.length - nullQtyCount; i++) {
+      final += "end if\n";
+    }
+
+    let unirExp = `\nbufferConc = bufferConc // lexeme\n`;
+    let initConcat = `
 bufferConc = ""
-carro = cursor`
-        let concNodos = '';
-        
-        for (let i = 0; i < node.exprs.length; i++) {
-            let previousExpr = node.exprs[i - 1];
-            let currentExpr = node.exprs[i];
-            let nextExpr = node.exprs[i + 1];
-            console.log("START"+concNum)
-            if ((currentExpr.expr instanceof Clase || currentExpr.expr instanceof Cadena) &&
-                (previousExpr && (previousExpr.expr instanceof Clase || previousExpr.expr instanceof Cadena)) &&
-                (nextExpr && (nextExpr.expr instanceof Clase || nextExpr.expr instanceof Cadena))) {
-                    if(currentExpr.qty){
-                        concNodos +=currentExpr.accept(this) +'\nend if\n'+unirExp+'\n ! Instancia de Clase o Cadena, con anterior y siguiente también siendo de Clase o Cadena1';
-                    }else{
-                concNum+=1
-                concNodos += currentExpr.accept(this) + unirExp+'\n ! Instancia de Clase o Cadena, con anterior y siguiente también siendo de Clase o Cadena1';
-                    }
-            } else if ((currentExpr.expr instanceof Clase || currentExpr.expr instanceof Cadena) &&
-                       (!nextExpr || !(nextExpr.expr instanceof Clase || nextExpr.expr instanceof Cadena)) &&
-                       (!previousExpr || !(previousExpr.expr instanceof Clase || previousExpr.expr instanceof Cadena))) {
+carro = cursor`;
+    let concNodos = "";
 
-                        concNodos += currentExpr.accept(this) + `\nlexeme = lexeme // '=' // '"'// \'${currentId}\' // '"'\nreturn \nend if\n ! Instancia de Clase o Cadena, con anterior y siguiente siendo otra cosa */2`;
-                            
-            } else if ((currentExpr.expr instanceof Clase || currentExpr.expr instanceof Cadena) &&
-                       (!nextExpr || !(nextExpr.expr instanceof Clase || nextExpr.expr instanceof Cadena)) &&
-                       (previousExpr && (previousExpr.expr instanceof Clase || previousExpr.expr instanceof Cadena))) {
-                        if(!currentExpr.qty){
-                            concNum+=1
-                            concNodos += currentExpr.accept(this) +this.getEndConcat(concNum)+ '\n ! Instancia de Clase o Cadena, con anterior siendo de Clase o Cadena y siguiente siendo otra cosa */3';
-                        }else{
-                        concNodos += currentExpr.accept(this) +'\nend if\n'+this.getEndConcat(concNum)+ '\n ! Instancia de Clase o Cadena, con anterior siendo de Clase o Cadena y siguiente siendo otra cosa */3';
-                    }
-                    concNum = 0;
-            } else if ((currentExpr.expr instanceof Clase || currentExpr.expr instanceof Cadena) &&
-                       (nextExpr && (nextExpr.expr instanceof Clase || nextExpr.expr instanceof Cadena)) &&
-                       (!previousExpr || !(previousExpr.expr instanceof Clase || previousExpr.expr instanceof Cadena))) {
-                        if(currentExpr.qty){
-                            concNodos += initConcat+currentExpr.accept(this) +'\nend if\n'+unirExp+'\n ! Instancia de Clase o Cadena, con anterior siendo otra cosa y siguiente siendo de Clase o Cadena */4';
-                        }else{
-                        concNum+=1
-                        concNodos += initConcat+currentExpr.accept(this) +unirExp+'\n ! Instancia de Clase o Cadena, con anterior siendo otra cosa y siguiente siendo de Clase o Cadena */4';
-                        }
-            } else if (currentExpr.expr instanceof Opciones){
-                cuantificar = false;
-                concNodos +="\n !EMPIEZA OP\n "+currentExpr.accept(this)+"\n !Termina OP\n";
-                
-            } else {
-                concNodos +=' ! Otra cosa */';
-            }
-            console.log("END"+concNum)
+    for (let i = 0; i < node.exprs.length; i++) {
+      let previousExpr = node.exprs[i - 1];
+      let currentExpr = node.exprs[i];
+      let nextExpr = node.exprs[i + 1];
+      console.log("START" + concNum);
+
+      if(currentExpr.expr.val){
+        if(currentExpr.expr.val.length == 1){
+            currentId = currentExpr.expr.val
+        }else{
+            currentId = ruleName
         }
-        
-        
-        
+      }
+      if (
+        (currentExpr.expr instanceof Clase ||
+          currentExpr.expr instanceof Cadena) &&
+        previousExpr &&
+        (previousExpr.expr instanceof Clase ||
+          previousExpr.expr instanceof Cadena) &&
+        nextExpr &&
+        (nextExpr.expr instanceof Clase || nextExpr.expr instanceof Cadena)
+      ) {
+        if (currentExpr.qty) {
+          concNodos +=
+            currentExpr.accept(this) +
+            "\nend if\n" +
+            unirExp +
+            "\n ! Instancia de Clase o Cadena, con anterior y siguiente también siendo de Clase o Cadena1";
+        } else {
+          concNum += 1;
+          concNodos +=
+            currentExpr.accept(this) +
+            unirExp +
+            "\n ! Instancia de Clase o Cadena, con anterior y siguiente también siendo de Clase o Cadena1";
+        }
+      } else if (
+        (currentExpr.expr instanceof Clase ||
+          currentExpr.expr instanceof Cadena) &&
+        (!nextExpr ||
+          !(
+            nextExpr.expr instanceof Clase || nextExpr.expr instanceof Cadena
+          )) &&
+        (!previousExpr ||
+          !(
+            previousExpr.expr instanceof Clase ||
+            previousExpr.expr instanceof Cadena
+          ))
+      ) {
+        concNodos +=
+          currentExpr.accept(this) +
+          `\nlexeme = '"' // get_symbol(lexeme) //'"' // '=' //  \'${currentId}\' \nreturn \nend if\n ! Instancia de Clase o Cadena, con anterior y siguiente siendo otra cosa */2`;
+      } else if (
+        (currentExpr.expr instanceof Clase ||
+          currentExpr.expr instanceof Cadena) &&
+        (!nextExpr ||
+          !(
+            nextExpr.expr instanceof Clase || nextExpr.expr instanceof Cadena
+          )) &&
+        previousExpr &&
+        (previousExpr.expr instanceof Clase ||
+          previousExpr.expr instanceof Cadena)
+      ) {
+        if (!currentExpr.qty) {
+          concNum += 1;
+          concNodos +=
+            currentExpr.accept(this) +
+            this.getEndConcat(concNum) +
+            "\n ! Instancia de Clase o Cadena, con anterior siendo de Clase o Cadena y siguiente siendo otra cosa */3";
+        } else {
+          concNodos +=
+            currentExpr.accept(this) +
+            "\nend if\n" +
+            this.getEndConcat(concNum) +
+            "\n ! Instancia de Clase o Cadena, con anterior siendo de Clase o Cadena y siguiente siendo otra cosa */3";
+        }
+        concNum = 0;
+      } else if (
+        (currentExpr.expr instanceof Clase ||
+          currentExpr.expr instanceof Cadena) &&
+        nextExpr &&
+        (nextExpr.expr instanceof Clase || nextExpr.expr instanceof Cadena) &&
+        (!previousExpr ||
+          !(
+            previousExpr.expr instanceof Clase ||
+            previousExpr.expr instanceof Cadena
+          ))
+      ) {
+        if (currentExpr.qty) {
+          concNodos +=
+            initConcat +
+            currentExpr.accept(this) +
+            "\nend if\n" +
+            unirExp +
+            "\n ! Instancia de Clase o Cadena, con anterior siendo otra cosa y siguiente siendo de Clase o Cadena */4";
+        } else {
+          concNum += 1;
+          concNodos +=
+            initConcat +
+            currentExpr.accept(this) +
+            unirExp +
+            "\n ! Instancia de Clase o Cadena, con anterior siendo otra cosa y siguiente siendo de Clase o Cadena */4";
+        }
+      } else if (currentExpr.expr instanceof Opciones) {
+        cuantificar = false;
+        concNodos +=
+          "\n !EMPIEZA OP\n " + currentExpr.accept(this) + "\n !Termina OP\n";
+      } else {
+        concNodos += " ! Otra cosa */";
+      }
+      console.log("END" + concNum);
+    }
 
-        
-return concNodos
-}
+    return concNodos;
+  }
 
+  visitExpresion(node) {
+    let generatedCode = node.expr.accept(this);
 
-    visitExpresion(node) {
-        let generatedCode = node.expr.accept(this);
-
-
-        if (node.qty && !(node.expr instanceof Opciones)) {
-            
-            switch (node.qty) {
-                case '*': // Cero o más
-                    return `
+    if (node.qty && !(node.expr instanceof Opciones)) {
+      switch (node.qty) {
+        case "*": // Cero o más
+          return `
 ! Inicializar variables
 buffer = ""
 count = 0
@@ -165,8 +261,8 @@ else
     if (allocated(lexeme)) deallocate(lexeme)
         allocate(character(len=len(buffer)) :: lexeme)
         lexeme = buffer`;
-                case '+': // Uno o más
-                    return `
+        case "+": // Uno o más
+          return `
 ! Inicializar variables
 buffer = ""
 count = 0
@@ -184,8 +280,8 @@ else
     if (allocated(lexeme)) deallocate(lexeme)
         allocate(character(len=len(buffer)) :: lexeme)
         lexeme = buffer`;
-                case '?': // Cero o uno
-                    return `
+        case "?": // Cero o uno
+          return `
 !! Inicializar variables
 buffer = ""
 count = 0
@@ -203,119 +299,135 @@ else
     if (allocated(lexeme)) deallocate(lexeme)
         allocate(character(len=len(buffer)) :: lexeme)
         lexeme = buffer`;
-                default:
-                    return `
+        default:
+          return `
                     ${generatedCode}`;
-            }
-        }
-
-        return `${generatedCode}`;
+      }
     }
 
-    visitCadena(node) {
-        currentId = node.val
-        if (node.isCase !== null) {
-            return `
-if (to_lower(input(cursor:cursor + ${node.val.length - 1})) == to_lower("${node.val}")) then
+    return `${generatedCode}`;
+  }
+
+  visitCadena(node) {
+    
+    
+    
+    if (node.isCase !== null) {
+      return `
+if (to_lower(input(cursor:cursor + ${node.val.length - 1})) == to_lower("${
+        node.val
+      }")) then
     if (allocated(lexeme)) deallocate(lexeme)
     allocate(character(len=${node.val.length}) :: lexeme)
     lexeme = input(cursor:cursor + ${node.val.length - 1})
     cursor = cursor + ${node.val.length}`;
-        } else {
-            return `
+    } else {
+      return `
 if ("${node.val}" == input(cursor:cursor + ${node.val.length - 1})) then
     if (allocated(lexeme)) deallocate(lexeme)
     allocate(character(len=${node.val.length}) :: lexeme)
     lexeme = input(cursor:cursor + ${node.val.length - 1})
     cursor = cursor + ${node.val.length}`;
-        }
     }
+  }
 
-    visitRango(node) {
-        return '';
-    }
-    
-    visitClase(node) {
-        const isCase = node.isCase !== null;
-        let conditions = ``;
-    
-        const charConditions = node.chars
-            .filter((char) => typeof char === 'string')
-            .map(char => {
-                if (isCase) {
-                    return `(input(i:i) == ${this.toAsciiString(char,0)} .or. input(i:i) == ${this.toAsciiString(char,1)})`;
-                } else {
-                    return `input(i:i) == ${this.toAsciiString(char,2)}`;
-                }
-            });
-    
-        const rangeConditions = node.chars
-            .filter((char) => char instanceof Rango)
-            .map(range => {
-                if (isCase) {
-                    return `((input(i:i) >= ${this.toAsciiString(range.bottom,0)} .and. input(i:i) <= ${this.toAsciiString(range.top,0)}) .or. &
-                            (input(i:i) >= ${this.toAsciiString(range.bottom,1)} .and. input(i:i) <= ${this.toAsciiString(range.top,1)}))`;
-                } else {
-                    return `(input(i:i) >= ${this.toAsciiString(range.bottom,2)} .and. input(i:i) <= ${this.toAsciiString(range.top,2)})`;
-                }
-            });
-    
-        conditions = charConditions.concat(rangeConditions).join(' .or. &\n');
-    
-        return `
+  visitRango(node) {
+    return "";
+  }
+
+  visitClase(node) {
+    const isCase = node.isCase !== null;
+    let conditions = ``;
+
+    const charConditions = node.chars
+      .filter((char) => typeof char === "string")
+      .map((char) => {
+        if (isCase) {
+          return `(input(i:i) == ${this.toAsciiString(
+            char,
+            0
+          )} .or. input(i:i) == ${this.toAsciiString(char, 1)})`;
+        } else {
+          return `input(i:i) == ${this.toAsciiString(char, 2)}`;
+        }
+      });
+
+    const rangeConditions = node.chars
+      .filter((char) => char instanceof Rango)
+      .map((range) => {
+        if (isCase) {
+          return `((input(i:i) >= ${this.toAsciiString(
+            range.bottom,
+            0
+          )} .and. input(i:i) <= ${this.toAsciiString(range.top, 0)}) .or. &
+                            (input(i:i) >= ${this.toAsciiString(
+                              range.bottom,
+                              1
+                            )} .and. input(i:i) <= ${this.toAsciiString(
+            range.top,
+            1
+          )}))`;
+        } else {
+          return `(input(i:i) >= ${this.toAsciiString(
+            range.bottom,
+            2
+          )} .and. input(i:i) <= ${this.toAsciiString(range.top, 2)})`;
+        }
+      });
+
+    conditions = charConditions.concat(rangeConditions).join(" .or. &\n");
+
+    return `
 i = cursor
 if (${conditions}) then
     lexeme = input(cursor:i)
     cursor = i + 1`;
-    }
-    
-    
-    visitIdentificador(node){
-        return '';
-    }
-    
-    visitPunto(node) {
-        return '';
-    }
-    visitFin(node) {
-        return '';
-    }
-    // Metodos auxiliares**************************************************************************
+  }
 
-toAsciiString(char,num) {
-    console.log(char)
+  visitIdentificador(node) {
+    return "";
+  }
+
+  visitPunto(node) {
+    return "";
+  }
+  visitFin(node) {
+    return "";
+  }
+  // Metodos auxiliares**************************************************************************
+
+  toAsciiString(char, num) {
+    console.log(char);
     const charMap = {
-        '\\t': 9,  
-        '\\n': 10, 
-        '\\r': 13, 
-        ' ': 32   
+      "\\t": 9,
+      "\\n": 10,
+      "\\r": 13,
+      " ": 32,
     };
 
-    
     if (char in charMap) {
-        return `char(${charMap[char]})`;
+      return `char(${charMap[char]})`;
     } else {
-        if (char >= 'A' && char <= 'Z' && num == 1) {
-            char = String.fromCharCode(char.charCodeAt(0) + 32);
-        }else if(char >= 'a' && char <= 'z' && num == 0){
-            char = String.fromCharCode(char.charCodeAt(0) - 32);
-        }
-            
-        return `char(${char.charCodeAt(0)})`;
+      if (char >= "A" && char <= "Z" && num == 1) {
+        char = String.fromCharCode(char.charCodeAt(0) + 32);
+      } else if (char >= "a" && char <= "z" && num == 0) {
+        char = String.fromCharCode(char.charCodeAt(0) - 32);
+      }
+
+      return `char(${char.charCodeAt(0)})`;
     }
+  }
 
-}
+  getEndIfs(num) {
+    let endIfs = "";
+    for (let i = 0; i < num; i++) {
+      endIfs += "\nend if";
+    }
+    return endIfs;
+  }
 
-
- getEndIfs(num){
-    let endIfs = ""
-    for (let i = 0; i < num; i++) 
-        { endIfs += '\nend if';}
-    return endIfs
- }
-
- getEndConcat(num){
-    console.log("NUM"+num)
+  getEndConcat(num) {
+    console.log("NUM" + num);
     let endConc = `
     bufferConc = bufferConc // lexeme
     
@@ -323,13 +435,11 @@ toAsciiString(char,num) {
         
         if (allocated(lexeme)) deallocate(lexeme)
         allocate(character(len=len(bufferConc)) :: lexeme)
-        lexeme = bufferConc // '=' // '"'// \'${currentId}\' // '"'
-        return
+        lexeme = '"'//get_symbol(bufferConc) //'"'// '=' // \'${currentId}\' 
     end if
-    ${this.getEndIfs(num)}`
+    ${this.getEndIfs(num)}`;
 
-    return endConc
- }
-
+    return endConc;
+  }
 
 }
